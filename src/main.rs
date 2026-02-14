d661284bc62533f70c298578045d39d329119477
Julius Michaelis <gitter@liftm.de>
2026-02-14 16:43:55 +0900

    arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+use std::ffi::CString;
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    #[clap(long)]
+    map_root_user: bool,
+
+    #[clap(required = true)]
+    exec: Vec<CString>,
+}
+
 fn main() {
-    use nix::unistd::execvp;
+    let Args {
+        map_root_user,
+        exec,
+    } = dbg!(clap::Parser::parse());
+    use nix::unistd::{execvp, getegid, geteuid};
     use std::ffi::CString;
+    let (uid, gid) = (geteuid(), getegid());
     netns();
+    if map_root_user {
+        map_root(uid, gid);
+    }
     lo_up().expect("failed to bring up loopback");
-    execvp::<CString>(&CString::new("nu").unwrap(), &[]).expect("Exec failed");
-    // execvp::<CString>(&CString::new("true").unwrap(), &[]).expect("Exec failed");
+    let exe = exec.get(0).expect("No exe path");
+    execvp::<CString>(exe, &exec).expect("Exec failed");
 }
 
 fn lo_up() -> Result<(), Box<dyn std::error::Error>> {
@@ -33,14 +52,12 @@ fn lo_up() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn netns() {
-    use nix::{
-        sched::{CloneFlags, unshare},
-        unistd::{getegid, geteuid},
-    };
-    use std::fs::write;
-    let uid = geteuid();
-    let gid = getegid();
+    use nix::sched::{CloneFlags, unshare};
     unshare(CloneFlags::CLONE_NEWNET | CloneFlags::CLONE_NEWUSER).expect("Unshare failed");
+}
+
+fn map_root(uid: nix::unistd::Uid, gid: nix::unistd::Gid) {
+    use std::fs::write;
     write("/proc/self/uid_map", format!("0 {uid} 1")).expect("User map failed");
     // necessary for gid_map
     write("/proc/self/setgroups", "deny").expect("Deny setgroup failed");
